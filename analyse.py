6ed8d2e4731fa9c40eb142fc12aa70bb6bdf9f26
clement <clement.come@student-cs.fr>
2021-03-28 18:18:54 +0200

    Adding neural network player

---
+++ analyse.py
@@ -0,0 +1,94 @@
+import json
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def read_game(path: str, return_player: bool = False) -> pd.DataFrame:
+    with open(path) as f:
+        dic_content = json.load(f)
+    player_list = dic_content["player_list"]
+    game_summary_list = dic_content["game_summary_list"]
+    winner_list_list = dic_content["winner_list_list"]
+    del dic_content
+    print(f"Number of games: {len(game_summary_list)}")
+    game_list = []
+    game_winner_list = []
+    turn_list = []
+    point_card_list = []
+    card_list = []
+    player_idx_list = []
+    win_list = []
+    for i, (game_summary, winner_list) in enumerate(
+        zip(game_summary_list, winner_list_list)
+    ):
+        for k, turn in enumerate(game_summary):
+            j_win = -1
+            turn_winner = turn["turn_winner"]
+            if turn_winner != "None":
+                j_win = player_list.index(turn_winner)
+            for j, player_card in enumerate(turn["player_card_list"]):
+                game_list.append(i)
+                game_winner_list.append(
+                    list(map(lambda winner: player_list.index(winner), winner_list))
+                )
+                turn_list.append(k)
+                point_card_list.append(turn["card"])
+                card_list.append(player_card)
+                player_idx_list.append(j)
+                win_list.append(1 if j_win == j else 0)
+    if return_player:
+        return (
+            player_list,
+            pd.DataFrame(
+                {
+                    "game": game_list,
+                    "turn": turn_list,
+                    "point_card": point_card_list,
+                    "card": card_list,
+                    "player_idx": player_idx_list,
+                    "win": win_list,
+                    "game_winner": game_winner_list,
+                }
+            ),
+        )
+    return pd.DataFrame(
+        {
+            "game": game_list,
+            "turn": turn_list,
+            "point_card": point_card_list,
+            "card": card_list,
+            "player_idx": player_idx_list,
+            "win": win_list,
+            "game_winner": game_winner_list,
+        }
+    )
+
+
+def plot_hist_win_by_card(df_game: pd.DataFrame):
+    df_win = df_game[df_game["win"] == 1]
+    fig, axes = plt.subplots(3, 5, figsize=(16, 8))
+    for i, value in enumerate(np.unique(df_win["point_card"])):
+        ax = axes[i // 5, i % 5]
+        df_value = df_win[df_win["point_card"] == value]["card"]
+        df_value.hist(ax=ax, bins=14)
+        ax.set_title(value)
+    plt.plot()
+
+
+def plot_hist_winner_by_card(df_game: pd.DataFrame):
+    df_win = df_game[
+        df_game.apply(lambda row: row["player_idx"] in row["game_winner"], axis=1)
+    ]
+    fig, axes = plt.subplots(3, 5, figsize=(16, 8))
+    for i, value in enumerate(np.unique(df_win["point_card"])):
+        ax = axes[i // 5, i % 5]
+        df_value = df_win[df_win["point_card"] == value]["card"]
+        df_value.hist(ax=ax, bins=15)
+        ax.set_title(value)
+    plt.plot()
+
+
+if __name__ == "__main__":
+    df = read_game("summary_2p_1000g_random_1.json")
+    print(df.head())
--- game.py
+++ game.py
@@ -1,9 +1,11 @@
 import numpy as np
 from time import time
+from tqdm import tqdm
 from typing import List, Dict, Union, Tuple
 import json
+import pickle
 
-from player import Player, RandomPlayer, MaxPlayer
+from player import Player, RandomPlayer, MaxPlayer, NNPlayer
 
 
 class Game:
@@ -69,10 +71,10 @@ class Game:
             else:
                 card_list = [card if card != m else 16 for card in card_list]
 
-    def turn(self, card: int) -> Tuple[Player, List[int]]:
+    def turn(self, card: int, turn: int) -> Tuple[Player, List[int]]:
         if self.verbose_:
             print("Card for this turn is:", card)
-        player_card_list = [player.play(card) for player in self.player_list_]
+        player_card_list = [player.play(card, turn) for player in self.player_list_]
         if self.verbose_:
             print("Cards played:")
             for player, player_card in zip(self.player_list_, player_card_list):
@@ -97,8 +99,8 @@ class Game:
     def play(self) -> Tuple[List[Player], List[Dict], List[Player]]:
         start_time = time()
         game = []
-        for card in self.card_list_:
-            turn_winner, player_card_list = self.turn(card)
+        for i, card in enumerate(self.card_list_):
+            turn_winner, player_card_list = self.turn(card, i)
             game.append(
                 {
                     "card": card,
@@ -130,18 +132,25 @@ class Game:
         return self.player_list_, game, winner_list
 
 
+# Playing several games
 if __name__ == "__main__":
-    game = Game(2, n_game=2, random=True, verbose=False)
+    n_player = 2
+    n_game = 1
+    game = Game(n_player, n_game=n_game, random=True, verbose=True)
+    nn_player_1 = NNPlayer("Tristan", learning_rate=0.6)
+    player_2 = Player("Eva")
+    # game.player_list_[0] = nn_player_1
+    game.player_list_[1] = player_2
     game_summary_list = []
     winner_list_list = []
-    while game.n_game_ > 0:
+    for _ in tqdm(range(n_game)):
         player_list, game_summary, winner_list = game.play()
         game_summary_list.append(game_summary)
         winner_list_list.append(list(map(str, winner_list)))
     # print(player_list)
     # print(game_summary_list)
     # print(winner_list_list)
-    with open("summary_2p_2g_random_1.json", "w") as f:
+    with open(f"summary_{n_player}p_{n_game}g_nn_1.json", "w") as f:
         json.dump(
             {
                 "player_list": list(map(str, player_list)),
@@ -151,3 +160,7 @@ if __name__ == "__main__":
             f,
             indent=4,
         )
+    # Saving the weights of neural network player
+    # with open(f"weights.pkl", "wb") as f:
+    #     pickle.dump({"A": nn_player_1.A_, "b": nn_player_1.b_}, f)
+
